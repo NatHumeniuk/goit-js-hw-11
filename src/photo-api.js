8ce2794e5fae9d7ba995739bc7d945fee3cfc529
NatHumeniuk <nat.gumenuk@gmail.com>
2023-11-12 20:50:31 +0200

    corrected Class

--- src/photo-api.js
+++ src/photo-api.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 export class PhotoApi {
-  static #KEY = '40580814-1f1887412e50f82259cec363c';
-  static #PER_PAGE = 40;
-  static #BASE_URL = 'https://pixabay.com/api/';
-
   constructor() {
     this.q = '';
     this.page = 1;
@@ -13,16 +9,16 @@ export class PhotoApi {
     this.safesearch = 'true';
   }
   async getPhotos() {
-    axios.defaults.baseURL = PhotoApi.#BASE_URL;
+    axios.defaults.baseURL = 'https://pixabay.com/api/';
     const config = {
       method: 'get',
       params: {
-        key: PhotoApi.#KEY,
+        key: '40580814-1f1887412e50f82259cec363c',
         q: this.q,
         image_type: this.type,
         orientation: this.orientation,
         safesearch: this.safesearch,
-        per_page: PhotoApi.#PER_PAGE,
+        per_page: 40,
         page: this.page,
       },
     };
